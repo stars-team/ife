2a923a6adf0aeb50d413be656cd39107303e3d53
wxj66666628 <w-x-0002@163.com>
2016-04-12 23:00:49 +0800

    modified comments task02_18_xjweng wxj66666628

--- task02/task02_18/task02_18_xjweng.js
+++ task02/task02_18/task02_18_xjweng.js
@@ -3,11 +3,17 @@
  */
 var num = new Array();
 
-//删除左右两端的空格
+/**
+ * 删除左右两端的空格
+ */
 function trim(str){
     return str.replace(/(^\s*)|(\s*$)/g, "");
 }
 
+/**
+ * 根据按钮类型入队
+ * @param btn_type
+ */
 function addNum(btn_type) {
     var num_in = trim(document.getElementById("num_in").value);
     console.log("addNum() action");
@@ -31,12 +37,20 @@ function addNum(btn_type) {
         return;
     }
 }
-
+/**
+ * 入队封装
+ * @param btn_type
+ */
 function addHandle(btn_type) {
     addNum(btn_type);
     renderQue();
+    //清空输入框
     document.getElementById("num_in").value = "";
 }
+/**
+ * 根据按钮类型出队
+ * @param btn_type
+ */
 function delHandle(btn_type) {
     if(btn_type === "left_out"){
             num.shift();
@@ -48,6 +62,9 @@ function delHandle(btn_type) {
     }
     renderQue();
 }
+/**
+ * 渲染队列
+ */
 function renderQue() {
     //先擦除记录
     var que = document.getElementById("queue");
@@ -63,15 +80,18 @@ function renderQue() {
     }
 
 }
+/**
+ * 初始化函数
+ */
 function init() {
-    //添加进入绑定事件
+    //初始化入队事件，点击时调用addHandle
     document.getElementById("_in").addEventListener("click",function(event){
         console.log(event.target.id);
         addHandle.call(null,event.target.id);
         console.log(num);
     });
 
-    //左右出
+    //左右出,点击时调用delHandle
     document.getElementById("_out").addEventListener("click",function(event){
         console.log(event.target.id);
         delHandle.call(null,event.target.id);
@@ -82,6 +102,7 @@ function init() {
     document.getElementById("queue").addEventListener("click",function(event){
         event.target.id = "sel_del";
         var que = document.getElementById("queue").getElementsByTagName("span");
+        //获取点击元素下标
         for(var v = 0; v < que.length; v++){
             if(que[v].id === "sel_del"){
                 delHandle.call(null,v);
